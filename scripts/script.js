6f5db6610d866065df4bd3cb0e8dd313d53e4c2b
Matthew Rae <mattyjrae123@hotmail.com>
2022-08-10 11:55:01 +1200

    Colour change to to black is incremental
    10% increase in alpha channel on each hover event
    on grid tiles, instead of going straight white to black

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,29 @@
-const gridItems = document.querySelectorAll('.grid-item');
+const gridItems = document.querySelectorAll(".grid-item");
 
+// Add eventListeners to all grid items
 for (let i = 0; i < gridItems.length; i += 1) {
-  gridItems[i].addEventListener('mouseover', (e) => {
-    gridItems[i].style.backgroundColor = 'black';
+  gridItems[i].addEventListener("mouseover", (e) => {
+
+    // current background colour of grid item
+    const newAlphaValue = getNewAlphaValue(gridItems[i]);
+
+    gridItems[i].style.backgroundColor = `rgba(0, 0, 0, ${newAlphaValue})`;
   });
+}
+
+function getNewAlphaValue(gridItem) {
+  // returns 'rgba()' string, or rgb() string if alpha value is already 1
+  const rgbaString = getComputedStyle(gridItem)
+                    .getPropertyValue("background-color");
+
+  // 14 is first index of rgba() alpha value, ")" is closing bracket
+  let alphaValue = rgbaString.substring(14, rgbaString.indexOf(")"));
+ 
+  alphaValue = parseFloat(alphaValue);
+  
+  if (alphaValue >= 1 || typeof alphaValue !== "number") {
+    return 1;
+  }
+
+  return alphaValue + 0.1;
 }
