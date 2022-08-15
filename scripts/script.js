32b5a63a1a6e6046aa0a03e68587bd62a8f08f7e
Matthew Rae <mattyjrae123@hotmail.com>
2022-08-16 11:39:50 +1200

    Remove redundant getNewAlphaValue() function

--- scripts/script.js
+++ scripts/script.js
@@ -72,29 +72,3 @@ function addGridTileEventListeners() {
     });
   }
 }
-
-/*
-  getNewAlphaValue()
-
-  Takes a grid item as parameter, gets it's current background-color
-  style as a string, and extracts the alpha value from the string. If
-  the current alpha value is between 0 and 0.9 inclusive, increments
-  alpha value and returns it. Otherwise, returns 1.
-
-function getNewAlphaValue(gridItem) {
-  // returns 'rgba()' string, or rgb() string if alpha value is already 1
-  const rgbaString = getComputedStyle(gridItem)
-                    .getPropertyValue("background-color");
-
-  // 14 is first index of rgba() alpha value, ")" is closing bracket
-  let alphaValue = rgbaString.substring(14, rgbaString.indexOf(")"));
- 
-  alphaValue = parseFloat(alphaValue);
-  
-  if (alphaValue >= 1 || typeof alphaValue !== "number") {
-    return 1;
-  }
-
-  return alphaValue + 0.1;
-}
-*/
